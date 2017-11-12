51d9994937e20a35095e88626fa233e6917b70b1
Iwrestledabeeronce <alyalyuhu@gmail.com>
2017-11-12 14:45:25 +0300

    .

--- metacritic/game_crawler.py
+++ metacritic/game_crawler.py
@@ -1,5 +1,4 @@
 import re
-import sys
 from game_item import Game
 from crawler import GameCrawler
 
@@ -7,7 +6,7 @@ def parse_game_page(response, crawler):
 	
 	def check(path):
 		tmp = response.xpath(path)
-		return tmp[0].strip() if len(tmp) > 0 else ''
+		return tmp[0].strip() if len(tmp) > 0 else 'tbd'
 		
 	game = Game()
 	
@@ -18,7 +17,7 @@ def parse_game_page(response, crawler):
 	game.metascore = check('//div[@class="section product_scores"]/div[@class="details main_details"]/div/div/a/div/span/text()')
 	game.userscore = check('//div[@class="section product_scores"]/div[@class="details side_details"]/div[@class="score_summary"]/div/a/div/text()')
 	game.user_count = check('//div[@class="section product_scores"]/div[@class="details side_details"]/div[@class="score_summary"]/div/div[@class="summary"]/p/span[@class="count"]/a/text()')	
-	game.user_count = '0' if game.user_count == '' else re.findall('\d+', game.user_count)[0]
+	game.user_count = '0' if game.user_count == 'tbd' else re.findall('\d+', game.user_count)[0]
 	game.developer = check('//li[@class="summary_detail developer"]/span[@class="data"]/text()')
 	game.rating = check('//li[@class="summary_detail product_rating"]/span[@class="data"]/text()')
 	game.genres = response.xpath('//li[@class="summary_detail product_genre"]/span[@class="data"]/text()')
@@ -38,17 +37,34 @@ def parse(response, crawler):
 				game = crawler.request('http://www.metacritic.com' + page[0].strip(), callback = parse_game_page)
 				
 		except KeyboardInterrupt:
-			crawler.exit()
+			crawler.exit('games.json')
 			
 	next_page = response.xpath('//span[@class="flipper next"]/a/@href')
 	if next_page:
 		return crawler.request(url = 'http://www.metacritic.com' + next_page[0].strip(), callback = parse)
 			
-urls = ['http://www.metacritic.com/browse/games/release-date/available/ps3/date']
+urls = [
+        'http://www.metacritic.com/browse/games/release-date/available/ps/date',
+        'http://www.metacritic.com/browse/games/release-date/available/ps2/date',
+        'http://www.metacritic.com/browse/games/release-date/available/ps3/date',
+        'http://www.metacritic.com/browse/games/release-date/available/playstation-4/date',
+        'http://www.metacritic.com/browse/games/release-date/available/xbox/date',
+        'http://www.metacritic.com/browse/games/release-date/available/xbox360/date',
+        'http://www.metacritic.com/browse/games/release-date/available/xbox-one/date',
+        'http://www.metacritic.com/browse/games/release-date/available/n64/date',
+        'http://www.metacritic.com/browse/games/release-date/available/gamecube/date',
+        'http://www.metacritic.com/browse/games/release-date/available/wii/date',
+        'http://www.metacritic.com/browse/games/release-date/available/wii-u/date',
+        'http://www.metacritic.com/browse/games/release-date/available/switch/date',
+        'http://www.metacritic.com/browse/games/release-date/available/gba/date',
+        'http://www.metacritic.com/browse/games/release-date/available/ds/date',
+        'http://www.metacritic.com/browse/games/release-date/available/3ds/date',
+        'http://www.metacritic.com/browse/games/release-date/available/psp/date',
+        'http://www.metacritic.com/browse/games/release-date/available/vita/date',
+        'http://www.metacritic.com/browse/games/release-date/available/dreamcast/date',
+        'http://www.metacritic.com/browse/games/release-date/available/pc/date'
+        ]
 
 crawler = GameCrawler()
-if len(sys.argv) > 1:
-	crawler.path = sys.argv[1]
-
 for url in urls:
 	crawler.request(url, parse)
