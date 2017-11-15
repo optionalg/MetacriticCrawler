293bb48d43a13367bc947a73ab8abb54c6a5d939
Iwrestledabeeronce <alyalyuhu@gmail.com>
2017-11-15 14:03:38 +0300

    hm

---
+++ metacritic/crawler.py
@@ -0,0 +1,135 @@
+from lxml import html
+import time
+import requests
+import json
+import signal
+import re
+from game_item import Game
+
+class Crawler:
+	
+	def __init__(self):
+		self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+		self.urls = [
+			'http://www.metacritic.com/browse/games/release-date/available/dreamcast/date',
+			'http://www.metacritic.com/browse/games/release-date/available/ps/date',
+			'http://www.metacritic.com/browse/games/release-date/available/ps2/date',
+			'http://www.metacritic.com/browse/games/release-date/available/ps3/date',
+			'http://www.metacritic.com/browse/games/release-date/available/ps4/date',
+			'http://www.metacritic.com/browse/games/release-date/available/xbox/date',
+			'http://www.metacritic.com/browse/games/release-date/available/xbox360/date',
+			'http://www.metacritic.com/browse/games/release-date/available/xboxone/date',
+			'http://www.metacritic.com/browse/games/release-date/available/n64/date',
+			'http://www.metacritic.com/browse/games/release-date/available/gamecube/date',
+			'http://www.metacritic.com/browse/games/release-date/available/wii/date',
+			'http://www.metacritic.com/browse/games/release-date/available/wii-u/date',
+			'http://www.metacritic.com/browse/games/release-date/available/switch/date',
+			'http://www.metacritic.com/browse/games/release-date/available/gba/date',
+			'http://www.metacritic.com/browse/games/release-date/available/ds/date',
+			'http://www.metacritic.com/browse/games/release-date/available/3ds/date',
+			'http://www.metacritic.com/browse/games/release-date/available/psp/date',
+			'http://www.metacritic.com/browse/games/release-date/available/vita/date',
+			'http://www.metacritic.com/browse/games/release-date/available/pc/date'
+			]
+		self.game_links = []
+		self.games = []
+		self.game_counter = 0
+		self.startup = time.time()
+		
+	def collect_links(self, result):
+		if not isinstance(result, str):
+			if result['next'] is not None:
+				self.urls.append(result['next'])
+			self.game_links += result['links']
+		else:
+			self.urls.append(result)
+			
+	def collect_games(self, result):
+		if not isinstance(result, str):
+			self.games.append(json.dumps(result, indent = 4))
+		else:
+			self.game_links.append(result)
+
+	def output(self, file):
+		while self.games:
+			file.write(',\n' + self.games.pop())
+			self.game_counter += 1
+			print('%4d games scrapped' %self.game_counter)
+			print('current execution time: %f' %(time.time() - self.startup))			
+
+	def print_list_links(self):
+		for i in range(len(self.urls)):
+                        print('%3d: %s' %(i, self.urls[i]))
+
+	def print_game_links(self):
+		for i in range(len(self.game_links)):
+                        print('%3d: %s' %(i, self.game_links[i]))
+
+	def download(self, url, retries = 0, callback = None):
+		signal.signal(signal.SIGINT, signal.SIG_IGN)
+		print ('downloading %s' %url)
+		try:
+			page = requests.get(url, headers = self.headers)
+			if page.status_code == requests.codes.ok:
+				if callback is not None:
+					return callback(html.fromstring(page.content), url)
+				else:
+					print('downloaded %s. and what now?' %url)
+			elif retries > 0:
+				time.sleep(0.1)
+				return self.download(url, retries - 1, callback)
+			else:
+				print ('Warning: bad response code. Dropping the request')
+				return url
+		except requests.exceptions.RequestException as e:
+			if retries > 0:
+				time.sleep(0.1)
+				return self.download(url, retries - 1, callback)
+			else:
+				print ('Error: unsolvable exception:\n"%s"\n' %e)
+				return url
+	
+	def game_list_parse(self, response, url):
+		if response is None:
+			return url
+		
+		ret = {'links': [], 'next': None}
+		
+		for item in response.xpath('//div[@class="product_wrap"]'):
+			page = item.xpath('div[@class="basic_stat product_title"]/a/@href')
+			if page:
+				ret['links'].append('http://www.metacritic.com' + page[0].strip())
+			else:
+				print('invalid page')
+				return url
+		
+		next_page = response.xpath('//span[@class="flipper next"]/a/@href')
+		if next_page:
+			ret['next'] = 'http://www.metacritic.com' + next_page[0].strip()
+		print('done')
+		return ret
+			
+	def game_page_parse(self, response, url):
+		if response is None:
+			return url
+			
+		def check(path):
+			tmp = response.xpath(path)
+			return tmp[0].strip() if len(tmp) > 0 else 'tbd'
+		
+		game = Game()
+                
+		game.title = check('//div[@class="product_title"]/a/span/h1/text()')
+		game.platform = check('//div[@class="product_title"]/span/a/span/text()|//div[@class="product_title"]/span/span/text()')
+		game.released = check('//li[contains(@class, "release_data")]/span[@class="data"]/text()')	
+		game.reviews_count = check('//div[@class="section product_scores"]/div[@class="details main_details"]/div/div/div[@class="summary"]/p/span[@class="count"]/a/span/text()')
+		game.metascore = check('//div[@class="section product_scores"]/div[@class="details main_details"]/div/div/a/div/span/text()')
+		game.userscore = check('//div[@class="section product_scores"]/div[@class="details side_details"]/div[@class="score_summary"]/div/a/div/text()')
+		game.user_count = check('//div[@class="section product_scores"]/div[@class="details side_details"]/div[@class="score_summary"]/div/div[@class="summary"]/p/span[@class="count"]/a/text()')	
+		game.user_count = '0' if game.user_count == 'tbd' else re.findall('\d+', game.user_count)[0]
+		game.developer = check('//li[@class="summary_detail developer"]/span[@class="data"]/text()')
+		game.rating = check('//li[@class="summary_detail product_rating"]/span[@class="data"]/text()')
+		game.genres = response.xpath('//li[@class="summary_detail product_genre"]/span[@class="data"]/text()')
+							
+		return game.__dict__
+
--- metacritic/mptest.py
+++ metacritic/mptest.py
@@ -1,97 +1,45 @@
 import multiprocessing as mp
-import signal
 import sys
-import time
-from crawler import TestCrawler
-
-urls = [
-		'http://www.metacritic.com/browse/games/release-date/available/dreamcast/date',
-		'http://www.metacritic.com/browse/games/release-date/available/ps/date',
-		'http://www.metacritic.com/browse/games/release-date/available/ps2/date',
-		'http://www.metacritic.com/browse/games/release-date/available/ps3/date',
-		'http://www.metacritic.com/browse/games/release-date/available/ps4/date',
-		'http://www.metacritic.com/browse/games/release-date/available/xbox/date',
-		'http://www.metacritic.com/browse/games/release-date/available/xbox360/date',
-		'http://www.metacritic.com/browse/games/release-date/available/xboxone/date',
-		'http://www.metacritic.com/browse/games/release-date/available/n64/date',
-		'http://www.metacritic.com/browse/games/release-date/available/gamecube/date',
-		'http://www.metacritic.com/browse/games/release-date/available/wii/date',
-		'http://www.metacritic.com/browse/games/release-date/available/wii-u/date',
-		'http://www.metacritic.com/browse/games/release-date/available/switch/date',
-		'http://www.metacritic.com/browse/games/release-date/available/gba/date',
-		'http://www.metacritic.com/browse/games/release-date/available/ds/date',
-		'http://www.metacritic.com/browse/games/release-date/available/3ds/date',
-		'http://www.metacritic.com/browse/games/release-date/available/psp/date',
-		'http://www.metacritic.com/browse/games/release-date/available/vita/date',
-		'http://www.metacritic.com/browse/games/release-date/available/pc/date'
-		]
-            
-game_links = []
-games = []
-	
-def product(base_list, *args):
-	ret = []
-	for item in base_list:
-		ret.append((item,) + tuple(args))
-	return ret
-
-def collect_links(result):
-	global urls
-	global game_links
-	if result['next'] is not None:
-		urls.append(result['next'])
-	game_links += result['links']
-
-def collect_games(result):
-	global games
-	if result is not None:
-		games.append(result)
-
+import platform
+from crawler import Crawler
+      
 def main():
+        out = open('output.json', 'w')
+        out.write('[\n')
+        crawler = Crawler()
+        print('crawler created')
+        if platform.system() == 'Windows':
+                mp.set_start_method('spawn')
+        else:
+                mp.set_start_method('fork')
+        processes = 8
+        pool = mp.Pool(processes = processes)
+        print('%d processes spawned' %processes)
+        counter = 0
+
+        def exit():
+                print('exiting...')
+                crawler.output(out)
+                out.write('\n]')
+                print('%d games crawled' %counter)
+                print('missed list urls:')
+                crawler.print_list_links()
+                print('missed game urls:')
+                crawler.print_game_links()
 	
-	startup = time.time()
-	out = open('output.json', 'w')
-	out.write('[\n')
-	crawler = TestCrawler()
-	mp.set_start_method('fork')
-	processes = 8
-	pool = mp.Pool(processes = processes)
-	counter = 0
-	
-	while True:
-		for i in range(processes):
-			if urls:
-				pool.apply_async(crawler.download, (urls.pop(), 100, crawler.game_list_parse), callback = collect_links)
-			elif game_links:
-				pool.apply_async(crawler.download, (game_links.pop(), 5, crawler.game_page_parse), callback = collect_games)
-			while games:
-				out.write(games.pop() + ',\n')
-				counter += 1
-				print('%4d games scrapped' %counter)
-				print('current_execution_time: %f' %(time.time() - startup))
-			
-			'''
-			dif = len(urls) - processes
-			if dif < 0:
-				print('ALARM\ncounter: %d' %counter)
-				#do something!
-				
-			results = pool.starmap(crawler.download, product(urls, 100, crawler.game_list_parse))
-			urls = []
-			for result in results:
-				if result is not None:
-					if result['next'] is not None:
-						print('I have the next link')
-						urls.append(result['next'])
-					else:
-						print('I have no next link')
-					game_links += result['links']
-			print('%d links crawled' %len(game_links))
-			print('current execution time: %f' %(time.time() - startup))
-			counter += 1
-		#	50916 links crawled
-		#	current execution time: 765.866702
-			'''
+        while True:
+                for i in range(processes):
+                        try:
+                                if crawler.urls:
+                                        pool.apply_async(crawler.download, (crawler.urls.pop(0), 100, crawler.game_list_parse), callback = crawler.collect_links)
+                                elif crawler.game_links:
+                                        pool.apply_async(crawler.download, (crawler.game_links.pop(0), 5, crawler.game_page_parse), callback = crawler.collect_games)
+                                crawler.output(out)
+                                
+                        except KeyboardInterrupt:
+                                print('keyboard interrupt catched')
+                                exit()
+                                raise KeyboardInterrupt
 			
 		
 if __name__ == '__main__':
